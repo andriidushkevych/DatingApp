222c9a48a118e6dfd495ec3da2b8c1d135dfa6ea
Andrii Dushkevych <andrey.dushkevich@gmail.com>
2019-03-06 14:32:17 -0500

    sorting added

--- DatingApp.API/Data/DatingRepository.cs
+++ DatingApp.API/Data/DatingRepository.cs
@@ -45,7 +45,7 @@ namespace DatingApp.API.Data
 
         public async Task<PagedList<User>> GetUsers(UserParams userParams)
         {
-            IQueryable<User> users = _context.Users.Include(p => p.Photos);
+            IQueryable<User> users = _context.Users.Include(p => p.Photos).OrderByDescending(u => u.LastActive);
             
             users = users.Where(u => u.Id != userParams.UserId);
             users = users.Where(u => u.Gender == userParams.Gender);
@@ -54,6 +54,19 @@ namespace DatingApp.API.Data
                 var minDateOfBirth = DateTime.Today.AddYears(-userParams.MaxAge - 1);
                 var maxDateOfBirth = DateTime.Today.AddYears(-userParams.MinAge);
                 users = users.Where(u => u.DateOfBirth >= minDateOfBirth && u.DateOfBirth <= maxDateOfBirth);
+            }
+
+            if(!string.IsNullOrEmpty(userParams.OrderBy)) 
+            {
+                switch (userParams.OrderBy)
+                {
+                    case "created":
+                        users = users.OrderByDescending(u => u.Created);
+                        break;
+                    default:
+                        users = users.OrderByDescending(u => u.LastActive);
+                        break;
+                }
             }
 
             return await PagedList<User>.CreateAsync(users, userParams.PageNumber, userParams.PageSize);
