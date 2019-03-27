280fb912512e052638271c7f4b3062df673b724e
Andrii Dushkevych <andrey.dushkevich@gmail.com>
2019-03-27 15:39:52 -0400

    User roles added to Seed

--- DatingApp.API/Data/Seed.cs
+++ DatingApp.API/Data/Seed.cs
@@ -14,23 +14,53 @@ namespace DatingApp.API.Data
     {
 
         private readonly UserManager<User> _userManager;
+        private readonly RoleManager<Role> _roleManager;
 
-        public Seed(UserManager<User> userManager)
+        public Seed(UserManager<User> userManager, RoleManager<Role> roleManager)
         {
+            _roleManager = roleManager;
             _userManager = userManager;
         }
 
         public void SeedUsers()
         {
-            if(!_userManager.Users.Any())
+            if (!_userManager.Users.Any())
             {
                 var userData = File.ReadAllText("Data/UserSeedData.json");
                 var users = JsonConvert.DeserializeObject<List<User>>(userData);
+
+                var roles = new List<Role>
+                {
+                    new Role {Name = "Member"},
+                    new Role {Name = "Admin"},
+                    new Role {Name = "Moderator"},
+                    new Role {Name = "VIP"}
+                };
+
+                foreach(var role in roles)
+                {
+                    _roleManager.CreateAsync(role).Wait();
+                }
+
                 foreach (var user in users)
                 {
                     _userManager.CreateAsync(user, "password").Wait();
+                    _userManager.AddToRoleAsync(user, "Member").Wait();
+                }
+
+                var adminUser = new User
+                {
+                    UserName = "admin"
+                };
+
+                IdentityResult result = _userManager.CreateAsync(adminUser, "admin").Result;
+
+                if (result.Succeeded)
+                {
+                    var admin = _userManager.FindByNameAsync("admin").Result;
+                    _userManager.AddToRolesAsync(admin, new[] {"Admin", "Moderator"}).Wait();
                 }
             }
-        }       
+        }
     }
 }
